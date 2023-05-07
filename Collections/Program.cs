b7d7958dc6b29722c45c249c98ccc3ac739db6b7
Francisco <franc.dev18@gmail.com>
2023-05-06 23:55:06 -0500

    Architecture, Collections and Types Collections in C#

---
+++ Collections/Program.cs
@@ -0,0 +1,49 @@
+﻿//Collections 
+
+//String Collections
+
+var Computers = new List<string>();
+
+Computers.Add ("HP");
+Computers.Add ("Dell");
+Computers.Add ("Lenovo");
+Computers.Add ("Toshiba");
+Computers.Add ("Acer");
+Computers.Add ("Asus");
+
+var GamingPc = new List<string>{"AlienWare", "MSI", "Predator", "Hp_Gaming" };
+
+
+foreach (var el in Computers)
+{
+    Console.WriteLine ( el + " ");
+}
+
+List<Element> element = CreateList(); 
+
+
+var query = from el in element where el.AtomicNum != 20 select el;
+foreach (Element el in query)
+Console.WriteLine ($"Name: {el.Name}, Simbol: {el.Simbol}, Atomic_Number: {el.AtomicNum}");
+
+
+static List<Element> CreateList()
+{
+    return new List<Element>
+    {
+        {new Element() {Simbol = "K", Name = "Potasio", AtomicNum = 19}},
+        {new Element() {Simbol = "Ca", Name = "Calcio", AtomicNum = 20}},
+        {new Element() {Simbol = "Na", Name = "Sodio", AtomicNum = 11}},
+
+    };
+}
+
+public class Element 
+{
+    public string Name {get; set;}
+    public string Simbol {get; set;}
+
+    public int AtomicNum { get; set;}
+}
+
+
---
+++ TypesCollections/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+//Generic List
+
+
+List<int> numbers = new List<int>();
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+
+numbers.Remove(5);
+
+foreach ( var Number in numbers)
+{ 
+    Console.WriteLine(Number);
+}
+
+
+Console.WriteLine("---HashTable---");
+//Hashtable
+
+Hashtable Num =  new Hashtable();
+Num.Add(1, "1");
+Num.Add(2, "2");
+Num.Add(3, "3");
+Num.Add(4, "4");
+
+foreach ( var Number in Num.Values)
+{ 
+    Console.WriteLine(Number);
+}
+
+
+Console.WriteLine("---Dictionary---");
+//Dictionary
+
+Dictionary<int, string> number = new Dictionary<int, string>();
+number.Add(11, "1");
+number.Add(22, "2");
+number.Add(32, "3");
+number.Add(44, "4");
+foreach ( var Number in number.Values)
+{ 
+    Console.WriteLine(Number);
+}
+
+
+//Colas 
+Console.WriteLine("---Queue---");
+
+Queue<int> Numbers  = new Queue<int>();
+Numbers.Enqueue(1);
+Numbers.Enqueue(12);
+Numbers.Enqueue(123);
+Numbers.Enqueue(1234);
+Numbers.Dequeue();
+
+foreach (var Number in Numbers)
+{
+    Console.WriteLine(Number);
+
+}
+
+
+//Pilas
+Console.WriteLine ("---Stack---");
+
+Stack<int> nums = new Stack<int>();
+nums.Push(7);
+nums.Push(5);
+nums.Push(10);
+nums.Push(2);
+
+int last = nums.Pop();
+
+foreach (var Numero in nums)
+{
+    Console.WriteLine(Numero);
+}
+
